924b5b10f23b8989f884bb0d8bab833c10a46520
han-labs <huynhgiahan680@gmail.com>
2025-11-02 22:13:43 +0700

    fix(db): use Supabase transaction pooler 6543 + reduce pool + cache categories 60s

--- utils/db.js
+++ utils/db.js
@@ -2,33 +2,29 @@
 import knex from 'knex';
 
 
+const DB_CONFIG = {
+    host: 'aws-1-ap-southeast-1.pooler.supabase.com',
+    port: 6543,
+    user: 'postgres.hcfyjxhpsvqtdgwounbo',
+    password: 'Abc@123*#**',
+    database: 'postgres',
+};
+
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'aws-1-ap-southeast-1.pooler.supabase.com',
-        port: 6543,
-        user: 'postgres.hcfyjxhpsvqtdgwounbo',
-        password: 'Abc@123*#**',
-        database: 'postgres',
+        ...DB_CONFIG,
         ssl: { rejectUnauthorized: false },
-        keepAlive: true
     },
     pool: {
         min: 0,
-        max: 6,
-        acquireTimeoutMillis: 15000,
+        max: 4,
+        acquireTimeoutMillis: 10000,
         idleTimeoutMillis: 10000,
-        createTimeoutMillis: 15000,
-        reapIntervalMillis: 2000,
+        createTimeoutMillis: 5000,
+        reapIntervalMillis: 1000,
         propagateCreateError: false,
-        // Thiết lập timeouts trong 1 connection ngay khi tạo
-        afterCreate: (conn, done) => {
-            conn.query("SET statement_timeout = '15s'; SET idle_in_transaction_session_timeout = '5s';", (err) => {
-                done(err, conn);
-            });
-        },
     },
-    debug: false,
 });
 
 
